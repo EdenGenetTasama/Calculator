94b05e332415b31ec111f6c13ae0185bd0917238
EdenGenetTasama <edentasama19@gmail.com>
2021-10-01 19:05:48 +0300

    .

--- main.js
+++ main.js
@@ -8,219 +8,95 @@ const hilokInput = document.getElementById("hilok");
 
 const Numbers = document.getElementsByClassName("numbers"); //
 
-// ההדפסה של המספרים
-// for (let i = 0; i < Numbers.length; i++) {
-//   Numbers[i].addEventListener("click", function () {
-//     inputOne.value += Numbers[i].innerText;
-//   });
-// }
-
-// for (let i = 0; i < Numbers.length; i++) {
-//   Numbers[i].oninput = function () {
-//     inputTwo.value += Numbers[i].innerText;
-//   };
-
 const inputButton = document.getElementById("button");
-
-// כפתורי פעולות חשבון
-
-// plusInput.addEventListener("click", function(){
-//     let sum= Number(inputOne.value) + Number(inputTwo.value);
-//     console.log(sum);
-// })
-
-// minusInput.addEventListener("click",function(){
-//     let sum= Number(inputOne.value)-Number(inputTwo.value);
-//     console.log(sum);
-// })
-
-// dubeleInput.addEventListener("click",function(){
-//     let sum=Number(inputOne.value)*Number(inputTwo.value);
-//     console.log(sum);
-// })
-
-// hilokInput.addEventListener("click",function(){
-//     let sum=Number(inputOne.value)/Number(inputTwo.value);
-//     console.log(sum);
-// })
-
-// let firstNum = 0;
-//   function saveFirstNum(params) {
-//     //
-//   }
 
 // הדפסה של הפעולות
 const answerOfMachvon = document.getElementById("answerOfMachvon");
 
-// plusInput.onclick = function () {
-//   let sum = Number(inputOne.value) + Number(inputTwo.value);
-//   answerOfMachvon.innerHTML = "הפיתרון הוא: " + sum;
-// };
+// Fix                          */
 
-// minusInput.onclick = function () {
-//   let sum = Number(inputOne.value) - Number(inputTwo.value);
-//   answerOfMachvon.innerHTML = "הפיתרון הוא: " + sum;
-// };
+//*define empty variable* */
 
-// dubeleInput.onclick = function () {
-//   let sum = Number(inputOne.value) * Number(inputTwo.value);
-//   answerOfMachvon.innerHTML = "הפיתרון הוא: " + sum;
-// };
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let point = false;
 
-// hilokInput.onclick = function () {
-//   let sum = Number(inputOne.value) / Number(inputTwo.value);
-//   answerOfMachvon.innerHTML = "הפיתרון הוא: " + sum;
-// };
+function addOperator(currentOperator) {
+  if (secondNumber != null) {
+    firstNumber = calculate();
+    secondNumber = null;
+  } else {
+    if (operator != null) {
+      console.log("ERROR");
+    }
+  }
+  operator = currentOperator;
+  // console.log(operator);
+}
 
-/*                     Fix                          */
+function calculatePrint() {
+  if (firstNumber == null || secondNumber == null || operator == null) {
+    console.log("ERROR");
+    return;
+  }
+  console.log(calculate());
+  firstNumber = calculate();
+  operator = null;
+  secondNumber = null;
+}
 
-function firstNumberValue(numberOne ,numberTwo ) {
-  let numberOneClick = 0;
+function calculate() {
+  switch (operator) {
+    case "+":
+      return firstNumber + secondNumber;
+      break;
+    case "-":
+      return firstNumber - secondNumber;
+      break;
+    case "*":
+      return firstNumber * secondNumber;
+      break;
 
-  switch (numberOne) {
-    case "1":
-      switch (numberTwo) {
-        case "+":
-          numberOneClick += numberOne+numberTwo
-          console.log(numberOneClick);
-          break;
-      
-        default:
-          break;
-      }
-      // numberOneClick += 1;
-      // console.log(numberOneClick);
-    
+    case "/":
+      return firstNumber / secondNumber;
       break;
-    case "2":
-      numberOneClick += 2;
-      console.log(numberOneClick);
-      break;
-    case "3":
-      numberOneClick += 3;
-      console.log(numberOneClick);
-      break;
-    case "4":
-      numberOneClick += 4;
-      console.log(numberOneClick);
-      break;
-    case "5":
-      numberOneClick += 5;
-      console.log(numberOneClick);
-      break;
-    case "6":
-      numberOneClick += 6;
-      console.log(numberOneClick);
-      break;
-    case "7":
-      numberOneClick += 7;
-      console.log(numberOneClick);
-      break;
-    case "8":
-      numberOneClick += 8;
-      console.log(numberOneClick);
-      break;
-    case "9":
-      numberOneClick += 9;
-      console.log(numberOneClick);
-      break;
-    case "0":
-      numberOneClick += 0;
-      console.log(numberOneClick);
-      break;
+
     default:
       break;
   }
-  return numberOneClick;
 }
 
-// function secondNumberValue(number) {
+function deleteNum() {
+  firstNumber = null;
+  secondNumber = null;
+  operator = null;
+  console.log(" ");
+}
 
-//   let numberTwoClick = 0;
-//   switch (number) {
-//     case "1":
-//       numberTwoClick += 1;
-//       console.log(numberTwoClick);
-//       break;
-//     case "2":
-//       numberTwoClick += 2;
-//       console.log(numberTwoClick);
-//       break;
-//     case "3":
-//       numberTwoClick += 3;
-//       console.log(numberTwoClick);
-//       break;
-//     case "4":
-//       numberTwoClick += 4;
-//       console.log(numberTwoClick);
-//       break;
-//     case "5":
-//       numberTwoClick += 5;
-//       console.log(numberTwoClick);
-//       break;
-//     case "6":
-//       numberTwoClick += 6;
-//       console.log(numberTwoClick);
-//       break;
-//     case "7":
-//       numberTwoClick += 7;
-//       console.log(numberTwoClick);
-//       break;
-//     case "8":
-//       numberTwoClick += 8;
-//       console.log(numberTwoClick);
-//       break;
-//     case "9":
-//       numberTwoClick += 9;
-//       console.log(numberTwoClick);
-//       break;
-//     case "0":
-//       numberTwoClick += 0;
-//       console.log(numberTwoClick);
-//       break;
-//     default:
-//       break;
-//   }
-//   return numberTwoClick
-// }
+/*חישוב* */
 
+function firstNumberValue(Num) {
+  if (operator == null) {
+    if (firstNumber == null) {
+      firstNumber = parseInt(Num);
+    } else {
+      if (point == false) {
+        firstNumber = firstNumber * 10 + parseInt(Num);
+      } else {
+        firstNumber += 
+      }
+    }
+  } else {
+    if (secondNumber == null) {
+      secondNumber = parseInt(Num);
+    } else {
+      secondNumber = secondNumber * 10 + parseInt(Num);
+    }
+  }
+}
+let counter = 1;
 
-
-let mathSing = document.getElementsByClassName("mathSing")
-
-
-// for (let i = 0; i < mathSing.length; i++) {
-// console.log(`${mathSing[i].value}`);
-// let sum = numberOneClick 
-// switch (mathSing[i].value) {
-//   case "+":
-//     console.log("2");
-//     break;
-//     case "-":
-//     console.log("-");
-//     break
-//     case "*":
-//     console.log("*");
-//     break
-//     case "/":
-//     console.log("/");
-//     break
-
-//   default:
-//     break;
-// }
-  
-// }
-// function mathSing(numberOneClick, numberTwoClick) {
-//   let sum = 1;
-//   switch (numberOneClick, numberTwoClick) {
-//     case "*":
-//         let sum = Number(numberOneClick.value) * Number(numberTwoClick.value);
-//         //   answerOfMachvon.innerHTML = "הפיתרון הוא: " + sum;
-//         console.log(sum);
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
+function dotCalculate() {
+  point = true;
+}
